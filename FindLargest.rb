52a6c810842a2bcbe39b6c472bbf51e14ceba631
bhargav <bhargav.kaneriya@gmail.com>
2014-04-15 14:56:26 +0530

    Added Comments to FindLargest.rb
    Added comments for better understanding

--- FindLargest.rb
+++ FindLargest.rb
@@ -1,9 +1,9 @@
 class FindLargest
-	def find_largest(a, *nums) //*nums is variable argument
-		largest = a
-		nums.each do |num| //for each num in nums
-			if(num > largest)
-				largest = num;
+	def find_largest(a, *nums) #*nums is variable argument
+		largest = a			   #assume the first nuumber is the largest
+		nums.each do |num|     #for each num in nums
+			if(num > largest)  #if current number is larger than largest
+				largest = num; #assign num to largest
 			end
 		end
 		return largest;
